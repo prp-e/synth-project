2a1769487e2eef92968fa0526e0fe51f6d7a2e5e
Muhammadreza Haghiri <haghiri75@gmail.com>
2018-10-14 13:35:53 +0330

    A few methods added

--- synth.py
+++ synth.py
@@ -3,5 +3,17 @@ from scipy.io.wavfile import write
 
 class Synth:
 
-    def __init__():
-        pass 
+    def __init__(self, freq, sps, duration_s):
+        self.freq = freq
+        self.sps  = sps  #Samples per second
+        self.duration_s = duration_s
+
+    def print_info(self):
+        print("Frequency:", self.freq)
+        print("Samples per second:", self.sps)
+        print("Duration:", self.duration_s, "seconds")
+
+
+
+synth = Synth(440, 44100, 5)
+synth.print_info()
