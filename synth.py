3b99c96678ac997ee5069262aa67fdf38f5adda9
Muhammadreza Haghiri <haghiri75@gmail.com>
2018-10-14 14:19:47 +0330

    Sine Wave - In Progress

--- synth.py
+++ synth.py
@@ -1,5 +1,8 @@
 import numpy as np 
 from scipy.io.wavfile import write
+import matplotlib.pyplot as plt 
+
+
 
 class Synth:
 
@@ -13,10 +16,18 @@ class Synth:
         print("Samples per second:", self.sps)
         print("Duration:", self.duration_s, "seconds")
 
-    def sine_wave_generator(self):
-        pass 
+    def sine_wave_generator(self, quiet_factor=0.5):
+        sample_number = np.arange(self.duration_s * self.sps)
+        waveform = np.sinc(np.sin(2 * np.pi * sample_number * self.freq / self.sps))
+        waveform_queit = waveform * quiet_factor 
+        waveform_integer = np.int16(waveform_queit * (2**32))
+
+        return waveform_integer
+    
 
 
 
-synth = Synth(440, 44100, 5)
+
+synth = Synth(440.0, 44100, 5.0)
 synth.print_info()
+print(synth.sine_wave_generator())
