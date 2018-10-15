8b1306d160bb097288ceb2deddc0b26037517cf4
Muhammadreza Haghiri <haghiri75@gmail.com>
2018-10-15 21:13:04 +0330

    AM Modulation - fixed

--- synth.py
+++ synth.py
@@ -19,23 +19,23 @@ class Synth:
 
     def sine_wave_generator(self):
         sample_number = np.arange(self.duration_s * self.sps)
-        waveform = np.sinc(np.sin(2 * np.pi * sample_number * self.freq / self.sps))
+        waveform = np.sin(2 * np.pi * sample_number * self.freq / self.sps)
         waveform_queit = waveform * self.quiet_factor 
-        waveform_integer = np.int16(waveform_queit * (2**15))
+        #waveform_integer = np.int16(waveform_queit * (2**15))
 
-        return waveform_integer
-'''
+        return waveform_queit
+
     def sine_wave_amplitude_modulation(self, modulator_freq=0.25, ac=1.0, ka=0.25):
-        modulator = Synth(modulator_freq, self.sps, self.duration_s, 0.3)
-        modulator = modulator.sine_wave_generator() 
-        envelope = (ac * (1.0 + ka * modulator)) * self.quiet_factor
-        modulated_signal = self.sine_wave_generator() * modulator 
-'''
+        t_samples = np.arange(self.sps * self.duration_s)
+        modulator = np.sin(2 * np.pi * t_samples / self.sps) 
+        envelope = ac * (1.0 + ka * modulator)
+        modulated_signal = self.sine_wave_generator() * envelope 
+
         return modulated_signal
 
     def write_to_file(self, name="synthesizedWave.wav"):
         #to be redefined later. 
-       return write(name, self.sps, self.sine_wave_generator())
+       return write(name, self.sps, self.sine_wave_amplitude_modulation())
     
 
 
